9647a79add81dec783901429393bf63d7ee9d631
tr3xxx <86425337+tr3xxx@users.noreply.github.com>
2021-08-21 02:27:01 +0200

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ async def check_fg_channel():
         db = sqlite3.connect("db.sqlite")
         cursor = db.cursor()
         cursor.execute(f"SELECT channel_id FROM fg_channel ")
-        result = cursor.fetchall()r
+        result = cursor.fetchall()
         if result is None:
             return
         else:
